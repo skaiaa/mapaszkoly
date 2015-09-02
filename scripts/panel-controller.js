c9ba0f2077ae1a2b727a12c018ec4ca84db4e130
not7CD <norbertszulc13@gmail.com>
2015-09-03 00:24:09 +0200

    Nowy plan.svg
    Poprawiona obsługa wyświetlanego planu oraz panelu bocznego. Lepsza
    responsywność

--- scripts/panel-controller.js
+++ scripts/panel-controller.js
@@ -1,5 +1,4 @@
 var Panel = Panel || {};
-
 //Obiekt odpowiadający za obsługę panelu czasu
 var Time = function () {
 	this.Initialize = function (config) {
@@ -10,8 +9,23 @@ var Time = function () {
 	this.Update = function (time) {
 	  //Akutalizuje stan przycsku przy dniach
 		$("#days").find("#"+time.day).button("toggle");
-    //Aktualizuje stan listy godzin lekcyjnych
+        //Aktualizuje stan listy godzin lekcyjnych
 		$("#lessons option[id='"+time.lesson+"']").attr("selected", "selected");
 	}
 }
 Panel.Time = new Time;
+
+var Position = function () {
+	this.Initialize = function () {
+		this.Update();
+		Plan.Levels.Initialize();
+		$('input[name="levels"]').change( function() {
+			  console.log($(this).val());
+			  Plan.Levels.SetLevel($(this).val());
+			});
+	};
+	this.Update = function() {
+		$('#levels').find('#'+Plan.Levels.actualLevel).button('toggle')
+	};
+}
+Panel.Position = new Position;
--- scripts/plan-controller.js
+++ scripts/plan-controller.js
@@ -1,7 +1,38 @@
-//Ładujemy svg na stronę
+var Plan = Plan || {};
+
+var Canvas = function() {
+	this.Update = function() {
+		Plan.svg.attr("width", $('#canvas').width())
+        		.attr("height", $('window').height());
+	};
+};
+Plan.Canvas = new Canvas;
+
+var Levels = function() {
+	this.actualLevel = 0; //from -1 to 3
+	this.Initialize = function() {
+		Plan.Levels.HideAll(0);
+		$("svg #level"+this.actualLevel).fadeIn(400);
+	};
+	this.HideAll = function(time) {
+		$( "g[id^='level']" ).fadeOut(time);
+	};
+	this.SetLevel = function(newLevel) {
+		if(this.actualLevel != newLevel) {
+			Plan.Levels.HideAll(600);
+			this.actualLevel = newLevel;
+			$("svg #level"+this.actualLevel).fadeIn(400);
+		}
+	};
+};
+Plan.Levels = new Levels;
+
+
 d3.xml("data/plan.svg", "image/svg+xml", function(xml) {
 	document.getElementById("canvas").appendChild(xml.documentElement);
-});
+	Plan.svg = d3.select('#canvas svg');
+	console.log($('#canvas').width());
+	Plan.Canvas.Update();
 
-//Zmieniamy jego wielkość
-$("#canvas").find("svg").css("height", $( window ).height());
+	afterLoad();
+});
